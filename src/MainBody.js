1adfd7bf4580c9cd95d626f718bafca021c6f2fa
Dhawal <dhawalsharma26@icloud.com>
2022-08-21 03:50:36 -0400

    done with info card

--- src/MainBody.js
+++ src/MainBody.js
@@ -4,7 +4,12 @@ function MainBody() {
     return (
         <div className='info-card-container'>
             <div className='left-card'>
-                hey
+                <img src='https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80'></img>
+                <h2>Dhawal Sharma</h2>
+                <div className='buttons-left-card'>
+                <button className='contact-button'><img/>Contact</button>
+                <button className='resume-button'>Resume</button>
+                </div>
                 </div>
                 <div className='right-card'>
                     hey my name is dhawal
