0c91d9d2497dc10d2cb69da2aa6f6e14d6b2187e
Dhawal Sharma <dhawalsharma26@icloud.com>
2022-08-30 02:53:28 -0400

    changed in the footer

--- src/MainBody.js
+++ src/MainBody.js
@@ -1,6 +1,7 @@
 import './index.css'
 function MainBody() {
     return (
+        <div>
         <div className='info-card-container'>
             <div className='left-card'>
                 <img className='profile-image' src='https://avatars.githubusercontent.com/u/28450025?v=4' alt='Dhawal Sharma'></img>
@@ -16,6 +17,13 @@ function MainBody() {
         <span>I am a Front-End Developer and love to develop mobile applications and websites.</span><br/>
 
                     </div>   
+        </div>
+        <div>
+           <p>As a Computer Science Engineer I have been exposed to many languages and found my interest in Frontend programming,
+            I like to design applications and websites and love to make the user experience more interactiveand simplified.
+            I have worked on numerous project during this time period.  
+           </p>
+        </div>
         </div>       
     )
 }
